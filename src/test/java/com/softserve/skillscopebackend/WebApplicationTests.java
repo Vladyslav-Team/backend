99bd8ff034089f9bbbef8f6bd550eb61fe5475db
SEM <samerx777222@gmail.com>
2023-03-09 21:16:24 +0100

    Initial commit

---
+++ src/test/java/com/softserve/skillscopebackend/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.skillscopebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
