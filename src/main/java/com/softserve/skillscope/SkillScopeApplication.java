fde65b1b56a1ad239311aea44955f9145c802ce9
SEM <samerx777222@gmail.com>
2023-03-09 21:16:24 +0100

    Initial commit

---
+++ src/main/java/com/softserve/skillscope/SkillScopeApplication.java
@@ -0,0 +1,12 @@
+package com.softserve.skillscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillScopeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SkillScopeApplication.class, args);
+	}
+
+}
