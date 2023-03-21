e4b11e7ca61153a342fa463cea7a4b3b7b628210
Illia Kyselov <qwertytwenty129@gmail.com>
2023-03-22 01:30:33 +0200

    Add data.sql file

--- src/main/java/com/softserve/skillscope/talentInfo/TalentInfoRepository.java
+++ src/main/java/com/softserve/skillscope/talentInfo/TalentInfoRepository.java
@@ -3,5 +3,5 @@ package com.softserve.skillscope.talentInfo;
 import com.softserve.skillscope.talentInfo.model.entity.TalentInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TalentInfoRepository extends JpaRepository<TalentInfo, Integer> {
+public interface TalentInfoRepository extends JpaRepository<TalentInfo, Long> {
 }
