3fbe7f4f070a6d519c4177d8dad8d29528ed05a7
Illia Kyselov <qwertytwenty129@gmail.com>
2023-03-22 15:08:39 +0200

    Wrote Unit tests for Talent/TalentInfo classes

--- src/main/java/com/softserve/skillscope/talentInfo/TalentInfoRepository.java
+++ src/main/java/com/softserve/skillscope/talentInfo/TalentInfoRepository.java
@@ -2,6 +2,14 @@ package com.softserve.skillscope.talentInfo;
 
 import com.softserve.skillscope.talentInfo.model.entity.TalentInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface TalentInfoRepository extends JpaRepository<TalentInfo, Long> {
+    TalentInfo findByPhone(String phone);
+    @Transactional
+    @Modifying
+    @Query("update TalentInfo t set t.phone = ?1 where t.phone = ?2")
+    void updatePhoneByPhone(String phone, String phone1);
 }
