14c189241eb1e060b523b8c33300d5743c456b05
PanfiDen <panfilovskijd@gmail.com>
2023-03-26 20:46:57 +0300

    Deleted implementation of TalentMapper and changed the signature of this interface, changed the signature of build and plugins

--- src/main/java/com/softserve/skillscope/mapper/TalentMapper.java
+++ src/main/java/com/softserve/skillscope/mapper/TalentMapper.java
@@ -3,7 +3,15 @@ package com.softserve.skillscope.mapper;
 import com.softserve.skillscope.talent.model.entity.Talent;
 import com.softserve.skillscope.talent.model.entity.TalentFlashcard;
 import org.mapstruct.Mapper;
-@Mapper
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TalentMapper {
-    TalentFlashcard toTalentFlashcard(Talent talent);
+    default TalentFlashcard toTalentFlashcard(Talent talent){
+        return new TalentFlashcard(talent.getId(),
+                talent.getTalentInfo().getImage(),
+                talent.getName(),
+                talent.getSurname(),
+                talent.getTalentInfo().getLocation());
+    }
 }
