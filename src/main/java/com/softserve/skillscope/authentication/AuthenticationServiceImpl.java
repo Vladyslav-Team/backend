767a725cf7bcecbb0a362985e8ffe991e7f9da46
SEM <samerx777222@gmail.com>
2023-04-04 15:48:47 +0200

    Added ability to edit own talent profile, code refactor.

--- src/main/java/com/softserve/skillscope/authentication/AuthenticationServiceImpl.java
+++ src/main/java/com/softserve/skillscope/authentication/AuthenticationServiceImpl.java
@@ -1,14 +1,16 @@
-package com.softserve.skillscope.config.authentication;
+package com.softserve.skillscope.authentication;
 
 import com.softserve.skillscope.exception.talentException.TalentAlreadyExistsException;
 import com.softserve.skillscope.exception.talentException.TalentNotFoundException;
 import com.softserve.skillscope.talent.TalentRepository;
-import com.softserve.skillscope.talent.model.response.JwtToken;
-import com.softserve.skillscope.talent.model.request.RegistrationRequest;
 import com.softserve.skillscope.talent.model.entity.Talent;
+import com.softserve.skillscope.talent.model.entity.TalentProperties;
+import com.softserve.skillscope.talent.model.request.RegistrationRequest;
+import com.softserve.skillscope.talent.model.response.JwtToken;
 import com.softserve.skillscope.talentInfo.model.entity.TalentInfo;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
@@ -23,6 +25,8 @@ import java.time.temporal.ChronoUnit;
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final JwtEncoder jwtEncoder;
     private final TalentRepository talentRepo;
+    private final TalentProperties talentProps;
+    private final PasswordEncoder passwordEncoder;
 
 
     @Override
@@ -34,12 +38,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .name(request.name())
                 .surname(request.surname())
                 .email(request.email())
-                .password(request.password())
+                .password(passwordEncoder.encode(request.password()))
                 .build();
 
         TalentInfo talentInfo = TalentInfo.builder()
                 .location(request.location())
-                .age(request.dateOfBirth())
+                .age(request.birthday())
                 .image(checkEmptyImage(request))
                 .experience("Not mention yet")
                 .build();
@@ -62,7 +66,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     }
 
     @Override
-    public JwtToken login(String username) {
+    public JwtToken signIn(String username) {
         Talent talent = talentRepo.findByEmail(username).orElse(null);
         if (talent == null) {
             throw new TalentNotFoundException();
@@ -80,6 +84,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private String checkEmptyImage(RegistrationRequest request) {
         return request.image() == null
-                ? "https://drive.google.com/uc?export=view&id=13ECMnYIRyH6RrXV_yLgvhwPz6aZIS8nd" : request.image();
+                ? talentProps.defaultImage() : request.image();
     }
 }
--- src/main/java/com/softserve/skillscope/talent/service/TalentService.java
+++ src/main/java/com/softserve/skillscope/talent/service/TalentService.java
@@ -1,10 +1,15 @@
 package com.softserve.skillscope.talent.service;
 
+import com.softserve.skillscope.talent.model.request.TalentEditRequest;
 import com.softserve.skillscope.talent.model.dto.TalentProfile;
 import com.softserve.skillscope.talent.model.response.GeneralTalentResponse;
+import com.softserve.skillscope.talent.model.response.TalentResponse;
+import jakarta.transaction.Transactional;
 
 public interface TalentService {
     GeneralTalentResponse getAllTalentsByPage(int page);
 
     TalentProfile getTalentProfile(Long talentId);
+
+    TalentResponse editTalentProfile(Long talentId, TalentEditRequest talentToUpdate);
 }
