31f39e1ca34e2e2bb06b0fd5879865d252781b8f
SEM <samerx777222@gmail.com>
2023-04-06 15:48:30 +0200

    Changed properties, added comments, changed inserts

--- src/main/resources/db/migration/V2__Insert_talent.sql
+++ src/main/resources/db/migration/V2__Insert_talent.sql
@@ -1,53 +1,80 @@
-INSERT INTO talent (email, password, name, surname) VALUES ('Maksym@hotmail.com', '234lk5234asD', 'Maksym', 'Khudoliy');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Maksym@hotmail.com', '$2a$08$y4OZBISMWhWvkiOpnP3Zc.MBNqq8fafbcisOAAQIPk8BSLJHZSMSi', 'Maksym', 'Khudoliy');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Oleksandr@gmail.com', '23422kl34', 'Oleksandr ', 'Butrym');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Oleksandr@gmail.com', '$2a$08$3O8fkl2OkvraWerr2zlQyO7Ygk44twuy/0MMb0l6.T08Zshwv22Eq', 'Oleksandr ', 'Butrym');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Vladyslav@gmail.com', '23422kl34', 'Vladyslav ', 'Khrychov');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Vladyslav@gmail.com', '$2a$08$zBFw3opC9B03bErwdyLk5uvw9c403dDKOgE946AK9cFCSThsk5DNu', 'Vladyslav ', 'Khrychov');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('samerx777888@gmail.com', '6K87LJ9H6K7L8JH9', 'Samer', 'Khomsi Kak');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('samerx777888@gmail.com', '$2a$08$LGWQnKjKAm63mZzcKrv2zOYcU4vk1JyZFfforgwnw7.DpZ1yVI7Ay', 'Samer', 'Khomsi Kak');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Illia7788@gmail.com', '6K87LJ9H6K7L8JH9', 'Illia', 'Kyselov');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Illia7788@gmail.com', '$2a$08$sa9EBs93cPq1wEcR3M0SJOfSqbLqvp4ygPJTIHWFum5sW5wXf8W1C', 'Illia', 'Kyselov');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Kyryl88@gmail.com', '6K87LJ9H6K7L21', 'Kyryl', 'Srebriakov');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Kyryl88@gmail.com', '$2a$08$Ri4973P5CPJSQNssE.Xq4ufw.EDI5Ny17VIkttU3MKSLHzeId2tLq', 'Kyryl', 'Srebriakov');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Kyryl88@gmail.com', '6K87LJ9H6K7L21', 'Denys', 'Panfilovskyi');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Kyryl88@gmail.com', '$2a$08$mLeXjd.HZ8VDdxYOFNyMs.NheO9s9IdX5YHjKeOwg3.S4qubhL05i', 'Denys', 'Panfilovskyi');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Oleksandra21@gmail.com', '6K87LJ9H6K7L21', 'Oleksandra', 'Hryshchenko');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Oleksandra21@gmail.com', '$2a$08$.75RyqFSNScksZ5h3ft.P.kC2ZVYWfxYzzOdVWefOtTyT5wAbmDq2', 'Oleksandra', 'Hryshchenko');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Kyryl88@gmail.com', '6K87LJ9H6K7L21', 'Daria ', 'Hontar');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Kyryl88@gmail.com', '$2a$08$EYQe6SVlapAQ4FRHWLmmV.TKVSYaNdYtWT6E32OarMq67Vx0y1LKa', 'Daria ', 'Hontar');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Yarik@stehr.com', '6K87LJ9H6K7L8JH9', 'Yarik', 'Usenko');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Yarik@stehr.com', '$2a$08$zaLVVivRmeM38TAWcCvk.OyUvYVc7xAmMkpMrP5oWs2/Quq8xEkIu', 'Yarik', 'Usenko');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Vladislav@stehr.com', '6K87LJ9H6K7L8JH9', 'Vladislav', 'Zabilskij');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Vladislav@stehr.com', '$2a$08$H9tLMT885vm3AjWvxwpI2uqBet3EoVmW9SgUQIZx7bg2ldBj/Zkgy', 'Vladislav', 'Zabilskij');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Olesia@stehr.com', '6K87LJ9H6K7L8JH9', 'Olesia', 'Polezhai');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Olesia@stehr.com', '$2a$08$eWA7Fs.6dyY/KLaaZ7hQ/.v6BsRDzgiUOozyjsZOry259lX9UkTde', 'Olesia', 'Polezhai');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Bohdan@stehr.com', '6K87LJ9H6K7L8JH9', 'Bohdan', 'Anashkin');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Bohdan@stehr.com', '$2a$08$jSJdd5MufL5RTrolqpn9B.0zLQG5QSvJjBWkd6utxQsUuPJwR1UhS', 'Bohdan', 'Anashkin');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Billy@stehr.com', '6K87LJ9H6K7L8JH9', 'Billy', 'Herrington');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Billy@stehr.com', '$2a$08$UcqWUQ5yRpwqSvkCO8Knfu24bDSK3PHkXJMeXowBXsJNop3wi02H6', 'Billy', 'Herrington');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Darkholme@sauer.biz', '2k4lj5h633jh6', 'Van', 'Darkholme');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Darkholme@sauer.biz', '$2a$08$M3opBQcGUoerxtfjlvcP/uGq89A2ENeLOlPNF1JfrHpWw1UaPTm2O', 'Van', 'Darkholme');
 
-INSERT INTO talent (email, password, name, surname) VALUES ('Steve@hotmail.com', 'jl3hb456kjb6mn345', 'Steve', 'Rambo');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Steve@hotmail.com', '$2a$08$lkW4.dytv6Q2g2gUArmz/ePjMGwyXX6DltykD/a7lHalmjII5OZIm', 'Steve', 'Rambo');
 
-INSERT INTO TALENT (email, password, name, surname) VALUES ('Sylvester@langworth.com', '2jk345hljh34l56', 'Sylvester', 'Stallone');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('Sylvester@langworth.com', '$2a$08$jo21h8gYg6mmUUZzqUAY8ubWKro94bWyasXdEyUxKXvOf8QrCYzja', 'Sylvester', 'Stallone');
 
-INSERT INTO TALENT (email, password, name, surname) VALUES ('morton67@cremin.net', 'kj34l5kj6h4j5hg', 'Dolph', 'Lundgren');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('morton67@cremin.net', '$2a$08$Hsgb2bUSyHWrp9gtaiDehOUZPGk37uXNCzmhZcGjlWdlqR2lJw5Em', 'Dolph', 'Lundgren');
 
-INSERT INTO TALENT (email, password, name, surname) VALUES ('daugherty.emanuel@gmail.com', 'klj24h56kjhg36457kj6hg', 'Van', 'Damme');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('daugherty.emanuel@gmail.com', '$2a$08$iPhon9yJPlv/gioXcvhxNOkyCAT6SWVzYbWgpdT2dewPdy4Jer9Ji', 'Van', 'Damme');
 
-INSERT INTO TALENT (email, password, name, surname) VALUES ('jordan14@dare.net', 'kj34h56ljh45765jkl', 'Arnold', 'Schwarzenegger');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('jordan14@dare.net', '$2a$08$iPhon9yJPlv/gioXcvhxNOkyCAT6SWVzYbWgpdT2dewPdy4Jer9Ji', 'Arnold', 'Schwarzenegger');
 
-INSERT INTO TALENT (email, password, name, surname) VALUES ('medhurst.kiera@bartell.com', 'lk587kmn67m8', 'Ricardo', 'Milos');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('medhurst.kiera@bartell.com', '$2a$08$QjLOg0eO92/7qMAM4/JYg.rhOW.oNIJnhNeFm3J5i6Noztq7nNgsa', 'Ricardo', 'Milos');
 
-INSERT INTO TALENT (email, password, name, surname) VALUES ('dusty@gmail.com', 'jkh456l7kjh4l5', 'Denys', 'Karpenko');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('dusty@gmail.com', '$2a$08$xi6Y0keWrkcKqbiRtdDUQugnIQ3CQZbCmCwnHGnhVj1exOel8hqNi', 'Denys', 'Karpenko');
 
-INSERT INTO TALENT (email, password, name, surname) VALUES ('llssgqi@mydefipet.live', '234m56n34lk5j7h54', 'Svyatoslav', 'Hryshchenkovskii');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('llssgqi@mydefipet.live', '$2a$08$fnW54FsIIsXYNHid9GvnYupzp81PRwqMvxzKbfeCZSLmM208ZDW76', 'Svyatoslav', 'Hryshchenkovskii');
 
-INSERT INTO TALENT (email, password, name, surname) VALUES ('vmpecko@femailtor.com', '45nb674n5b67,nb', 'Vitalii', 'Kharechko');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('vmpecko@femailtor.com', '$2a$08$AxWhtDD.wU6qqNzICBzndu7jV6VgyTCaoP31KCWFq0NTC0qn40ZQe', 'Vitalii', 'Kharechko');
 
-INSERT INTO TALENT (email, password, name, surname) VALUES ('aypztmpcovdzylqkd@gmail.com', '23n54gbkj3254ngmn', 'Ayton', 'Crouford');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('aypztmpcovdzylqkd@gmail.com', '$2a$08$ymu9fo3eT4BhXqY7Tzlds.LoglMOIf5nbKRuluyvA3gyIFyF5C/6a', 'Ayton', 'Crouford');
 
-INSERT INTO TALENT (email, password, name, surname) VALUES ('dustofappearance678@gmail.com', '2345bg6kmnb34', 'Leonardo', 'Biller');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('dustofappearance678@gmail.com', '$2a$08$ymu9fo3eT4BhXqY7Tzlds.LoglMOIf5nbKRuluyvA3gyIFyF5C/6a', 'Leonardo', 'Biller');
 
-INSERT INTO TALENT (email, password, name, surname) VALUES ('alexandercaripo631@gmail.com', 'jh34lkj65hb3jn54b', 'Alexander', 'Caripo');
+INSERT INTO talent (email, password, name, surname)
+VALUES ('alexandercaripo631@gmail.com', '$2a$08$Pr0ll9mnfP4YkmpMrAOJReB9x.PMISDeS0nqO40FrRNJ6mYaX3pFK', 'Alexander', 'Caripo');
