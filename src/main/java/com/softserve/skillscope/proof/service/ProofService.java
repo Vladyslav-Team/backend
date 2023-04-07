61bf5080946c11889dbb7c79ca62ceb033e2d9aa
PanfiDen <panfilovskijd@gmail.com>
2023-04-07 17:53:00 +0300

    Change sql-query for table Proof + delete Override in ProofService

--- src/main/java/com/softserve/skillscope/proof/service/ProofService.java
+++ src/main/java/com/softserve/skillscope/proof/service/ProofService.java
@@ -1,8 +1,4 @@
 package com.softserve.skillscope.proof.service;
 
-import com.softserve.skillscope.proof.model.entity.Proof;
-import com.softserve.skillscope.proof.model.response.ProofStatus;
-
 public interface ProofService {
-    ProofStatus setProofStatus(ProofStatus status);
 }
--- src/main/java/com/softserve/skillscope/proof/service/ProofServiceImpl.java
+++ src/main/java/com/softserve/skillscope/proof/service/ProofServiceImpl.java
@@ -1,19 +1,15 @@
 package com.softserve.skillscope.proof.service;
 
 import com.softserve.skillscope.proof.ProofRepository;
-import com.softserve.skillscope.proof.model.entity.Proof;
 import com.softserve.skillscope.proof.model.response.ProofStatus;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
 
 @Service
 @AllArgsConstructor
 public class ProofServiceImpl implements ProofService{
     private ProofRepository proofRepo;
 
-    @Override
     public ProofStatus setProofStatus(ProofStatus status) {
         for(ProofStatus validProofStatus : ProofStatus.values()){
             if(validProofStatus == status){
