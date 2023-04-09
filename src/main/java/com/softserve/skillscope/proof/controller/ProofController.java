7b3756b6146e636b547a7621d614e1ae6aafc95c
SEM <samerx777222@gmail.com>
2023-04-09 16:11:23 +0200

    Bug fix that's not bug fix

--- src/main/java/com/softserve/skillscope/proof/controller/ProofController.java
+++ src/main/java/com/softserve/skillscope/proof/controller/ProofController.java
@@ -4,21 +4,31 @@ import com.softserve.skillscope.proof.model.dto.FullProof;
 import com.softserve.skillscope.proof.model.response.GeneralProofResponse;
 import com.softserve.skillscope.proof.service.ProofService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/proofs")
 public class ProofController {
     private ProofService proofService;
 
-    @GetMapping("{proof-id}")
+    @GetMapping("/proofs/{proof-id}")
     public FullProof showFullProof(@PathVariable("proof-id") Long proofId) {
         return proofService.getFullProof(proofId);
     }
 
-    @GetMapping
-    public GeneralProofResponse showAllProofs(@RequestParam(defaultValue = "1") int page, boolean newest){
+    @GetMapping("/proofs")
+    public GeneralProofResponse showAllProofs(@RequestParam(defaultValue = "1") int page, boolean newest) {
         return proofService.getAllProofByPage(page, newest);
+    }
+
+    @GetMapping("/talents/{talent-id}/proofs")
+    @ResponseStatus(HttpStatus.OK)
+    public GeneralProofResponse showAllProofsByTalentId(@PathVariable("talent-id") Long talentId,
+                                                        @RequestParam(defaultValue = "1") Optional<Integer> page,
+                                                        Optional<Boolean> sortByNewest) {
+        return proofService.getAllProofByPage(page.orElse(1), sortByNewest.orElse(true));
     }
 }
