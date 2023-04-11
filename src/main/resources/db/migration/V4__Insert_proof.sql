c19a085472310c01da75d4532bd7ecf29dc387e8
PanfiDen <panfilovskijd@gmail.com>
2023-04-11 12:13:14 +0300

    Update values in Proof table

--- src/main/resources/db/migration/V4__Insert_proof.sql
+++ src/main/resources/db/migration/V4__Insert_proof.sql
@@ -1,1 +1,84 @@
-DELETE FROM proof;
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 1);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 2);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 2);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 3);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 3);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 3);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 4);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 4);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 4);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 4);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 5);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 5);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 5);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 5);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 5);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 6);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 6);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 6);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 6);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 6);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 6);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 7);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 8);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 9);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 10);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 11);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 12);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 13);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 14);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 15);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 16);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 17);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 18);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 19);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 20);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 21);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 22);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 23);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 24);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 25);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 26);
+INSERT INTO proof (publication_date, title, description, status, talent_id)
+VALUES ('1999-05-23', 'testTitle', 'TestDescription', 'PUBLISHED', 27);
