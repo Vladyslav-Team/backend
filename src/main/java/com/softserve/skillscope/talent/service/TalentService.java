5dfa2fa35ac5cb7e6ff8394fdfb020b72ebbe872
PanfiDen <panfilovskijd@gmail.com>
2023-05-17 19:51:42 +0300

    Added ability to add Skills to Talent profile and delete skill from Talent profile

--- src/main/java/com/softserve/skillscope/talent/service/TalentService.java
+++ src/main/java/com/softserve/skillscope/talent/service/TalentService.java
@@ -1,6 +1,7 @@
 package com.softserve.skillscope.talent.service;
 
 import com.softserve.skillscope.general.model.GeneralResponse;
+import com.softserve.skillscope.skill.model.request.AddSkillsRequest;
 import com.softserve.skillscope.talent.model.request.TalentEditRequest;
 import com.softserve.skillscope.talent.model.dto.TalentProfile;
 import com.softserve.skillscope.talent.model.response.GeneralTalentResponse;
@@ -14,4 +15,8 @@ public interface TalentService {
     GeneralResponse editTalentProfile(Long talentId, TalentEditRequest talentToUpdate);
 
     ImageResponse getTalentImage(Long talentId);
+
+    GeneralResponse addSkillsOnTalentProfile(Long talentId, AddSkillsRequest newSkills);
+
+    GeneralResponse deleteSkillFromTalentProfile(Long talentId, Long skillId);
 }
